56ac32b8f72b0b2a86e682b69c7edf0afd9f2099
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2024-11-03 11:23:42 +0300

    Initial Commit

---
+++ SegmentTrain/SegmentTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentTrainApp.swift
+//  SegmentTrain
+//
+//  Created by Данил Толстиков on 03.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SegmentTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SegmentTrainTests/SegmentTrainTests.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentTrainTests.swift
+//  SegmentTrainTests
+//
+//  Created by Данил Толстиков on 03.11.2024.
+//
+
+import Testing
+@testable import SegmentTrain
+
+struct SegmentTrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
